fb18b2ae6b9ac2ae008ba3fc8b4ee4c936778e6a
triggerKr <trigger7@naver.com>
2024-03-15 00:13:15 +0900

    comparisonChart추가

--- src/main/resources/static/ui/js/chart.js
+++ src/main/resources/static/ui/js/chart.js
@@ -1,4 +1,7 @@
 
+
+	const colorRange= ['#FE2371','#544FC5','#2CAFFE','#2CAFFE','#FE6A35','#6B8ABC','#1C74BD','#1C74BD','#00A6A6','#D568FB']
+	
     Highcharts.setOptions({
         colors: ['#27187F', '#F29423']
     });
@@ -133,4 +136,109 @@ function _treemapChart(target, colorAxis, treemapData, func){
 	    
 	})
 
+}
+
+function _comparisonChart (target, param){
+	
+	const thisMonth =	param.thisMonth;
+	const lastMonth =	param.lastMonth;
+	const data 		=	param.data;
+	const dataPrev 	=	param.dataPrev;
+	const xAxisData =	param.xAxisData;
+		
+	for (const [key, value] of Object.entries(xAxisData)) {
+	    value.ucCode = key.toUpperCase();
+	}
+	
+	const getData = data => data.map(point => ({
+	    name: point[0],
+	    y: point[1],
+	    color: xAxisData[point[0]].color
+	}));
+	
+	const chart = Highcharts.chart(target, {
+	    chart: {
+	        type: 'column'
+	    },
+	    xAxisData,
+	    title: {
+	        text: '',
+	        align: 'left'
+	    },
+	    subtitle: {
+	        text: '',
+	        align: 'left'
+	    },
+	    plotOptions: {
+	        series: {
+	            grouping: false,
+	            borderWidth: 0
+	        }
+	    },
+	    legend: {
+	        enabled: false
+	    },
+	    tooltip: {
+	        shared: true,
+	        headerFormat: '<span style="font-size: 15px">' +
+	            '{point.key}' +
+	            '</span><br/>',
+	        pointFormat: '<span style="color:{point.color}">\u25CF</span> ' +
+	            '{series.name}: <b>{point.y} 억</b><br/>'
+	    },
+	    xAxis: {
+	        type: 'category',
+	        accessibility: {
+	            description: 'xAxisData'
+	        },
+	        max: 12,
+	        labels: {
+	            useHTML: true,
+	            animate: true,
+	            format: '{value}<br>' +
+	                '<span class="f32">' +
+	                '<span style="display:inline-block;height:32px;vertical-align:text-top;" ' +
+	                'class="flag {value}"></span></span>',
+	            style: {
+	                textAlign: 'center'
+	            }
+	        }
+	    },
+	    yAxis: [{
+	        title: {
+	            text: ''
+
+	        },
+ 			format: '{value}억',
+	        showFirstLabel: false
+	    }],
+	    series: [
+		{
+	        name: lastMonth,
+	        color: 'rgba(158, 159, 163, 0.5)',
+	      //  pointPlacement: -0.2,
+	        linkedTo: 'main',
+	        data: dataPrev["months"].slice()
+	    }, 
+		{
+	        name: thisMonth,
+	        id: 'main',
+	        dataSorting: {
+	            enabled: true,
+	            matchByName: true
+	        },
+	        dataLabels: [{
+	            enabled: true,
+	            inside: true,
+	            style: {
+	                fontSize: '16px'
+	            }
+	        }],
+	        data: getData(data["months"]).slice()
+	    }],
+	    exporting: {
+	        allowHTML: true
+	    }
+	});
+	
 }
