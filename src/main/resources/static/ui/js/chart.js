ec4af0b4399533b154e5a4d226610535721bd8e8
triggerKr <trigger7@naver.com>
2024-03-15 21:04:04 +0900

    순매출성장률

--- src/main/resources/static/ui/js/chart.js
+++ src/main/resources/static/ui/js/chart.js
@@ -1,7 +1,14 @@
 
 
-	const colorRange= ['#FE2371','#544FC5','#2CAFFE','#2CAFFE','#FE6A35','#6B8ABC','#1C74BD','#1C74BD','#00A6A6','#D568FB']
+	const _monthArray	= ['1월', '2월', '3월', '4월', '5월', '6월','7월', '8월', '9월', '10월', '11월', '12월'];
+	const _colorRange	= ['#FE2371','#544FC5','#2CAFFE','#2CAFFE','#FE6A35','#6B8ABC','#1C74BD','#1C74BD','#00A6A6','#D568FB']
 	
+	function _getRandomColor() {
+		
+	    const randomIndex = Math.floor(Math.random() * colorRange.length);
+	    return colorRange[randomIndex];
+	}
+
     Highcharts.setOptions({
         colors: ['#27187F', '#F29423']
     });
@@ -145,7 +152,7 @@ function _comparisonChart (target, param){
 	const data 		=	param.data;
 	const dataPrev 	=	param.dataPrev;
 	const xAxisData =	param.xAxisData;
-		
+
 	for (const [key, value] of Object.entries(xAxisData)) {
 	    value.ucCode = key.toUpperCase();
 	}
@@ -156,7 +163,7 @@ function _comparisonChart (target, param){
 	    color: xAxisData[point[0]].color
 	}));
 	
-	const chart = Highcharts.chart(target, {
+	Highcharts.chart(target, {
 	    chart: {
 	        type: 'column'
 	    },
@@ -191,7 +198,7 @@ function _comparisonChart (target, param){
 	        accessibility: {
 	            description: 'xAxisData'
 	        },
-	        max: 12,
+	        max: xAxisData.length,
 	        labels: {
 	            useHTML: true,
 	            animate: true,
@@ -216,7 +223,7 @@ function _comparisonChart (target, param){
 		{
 	        name: lastMonth,
 	        color: 'rgba(158, 159, 163, 0.5)',
-	      //  pointPlacement: -0.2,
+	        pointPlacement: -0.3,
 	        linkedTo: 'main',
 	        data: dataPrev["months"].slice()
 	    }, 
@@ -229,9 +236,9 @@ function _comparisonChart (target, param){
 	        },
 	        dataLabels: [{
 	            enabled: true,
-	            inside: true,
+	            inside: false,
 	            style: {
-	                fontSize: '16px'
+	                fontSize: '10px'
 	            }
 	        }],
 	        data: getData(data["months"]).slice()
@@ -241,4 +248,86 @@ function _comparisonChart (target, param){
 	    }
 	});
 	
+}
+
+function _lineAndColumn_chart(target){
+	
+	Highcharts.chart(target, {
+    chart: {
+        zoomType: 'xy'
+    },
+    title: {
+        text: 'Karasjok weather, 2021',
+        align: 'left'
+    },
+    subtitle: {
+        text: '',
+        align: 'left'
+    },
+    xAxis: [{
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+        labels: {
+            format: '{value}°C',
+            style: {
+                color: Highcharts.getOptions().colors[1]
+            }
+        },
+        title: {
+            text: 'Temperature',
+            style: {
+                color: Highcharts.getOptions().colors[1]
+            }
+        }
+    }, { // Secondary yAxis
+        title: {
+            text: 'Precipitation',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        },
+        labels: {
+            format: '{value} mm',
+            style: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        },
+        opposite: true
+    }],
+    tooltip: {
+        shared: true
+    },
+    legend: {
+        align: 'left',
+        x: 80,
+        verticalAlign: 'top',
+        y: 60,
+        floating: true,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || // theme
+            'rgba(255,255,255,0.25)'
+    },
+    series: [{
+        name: 'Precipitation',
+        type: 'column',
+        yAxis: 1,
+        data: [27.6, 28.8, 21.7, 34.1, 29.0, 28.4, 45.6, 51.7, 39.0,
+            60.0, 28.6, 32.1],
+        tooltip: {
+            valueSuffix: ' mm'
+        }
+
+    }, {
+        name: 'Temperature',
+        type: 'spline',
+        data: [-13.6, -14.9, -5.8, -0.7, 3.1, 13.0, 14.5, 10.8, 5.8,
+            -0.7, -11.0, -16.4],
+        tooltip: {
+            valueSuffix: '°C'
+        }
+    }]
+});
 }
