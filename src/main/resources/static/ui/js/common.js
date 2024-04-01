50ef96d1c53bff4b8ac9ca9077f7b1a66672e0b7
triggerKr <trigger7@naver.com>
2024-04-01 16:35:38 +0900

    .

--- src/main/resources/static/ui/js/common.js
+++ src/main/resources/static/ui/js/common.js
@@ -23,20 +23,22 @@ function _numberWithCommas (num) {
 
 function _chkHandler(pageId, page)
 {
-			
-    $("#"+pageId+"_ckAll").on('click',function() {
+		
+    $("#"+pageId+"_ckAll").off().on('click',function() {
 
     	const isAllChk =  $(this).prop("checked");
 		$("[id^='"+pageId+"_ck_']").prop("checked", isAllChk);
 		
     	const ckboxes = $("[id^='"+pageId+"_ck_']");
-	    pageId.checkedItems = isAllChk ? ckboxes.map(function() { return this.value; }).get() : [];
+	    page.checkedItems = isAllChk ? ckboxes.map(function() { return $(this).attr("id").replace(pageId+"_ck_",""); }).get() : [];
 
     });
-    $("[id^='"+pageId+"_ck_']").on('click',function() {
 
-	        if ($(this).prop("checked")){page.checkedItems.push($(this).val());}
-    	else { page.checkedItems = page.checkedItems.filter(item => item !== $(this).val());}  
+    $("[id^='"+pageId+"_ck_']").off().on('click',function() {
+
+		const index = $(this).attr("id").replace(pageId+"_ck_","");
+		if ($(this).prop("checked")){page.checkedItems.push(index);}
+    	else { page.checkedItems = page.checkedItems.filter(item => item !== index);}  
         
         const $checkboxSelector = $("[id^='"+pageId+"_ck_']")
         const allCheckboxChecked = $checkboxSelector.length === $checkboxSelector.filter(":checked").length;
