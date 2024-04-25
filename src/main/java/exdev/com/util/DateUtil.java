48e5d4f45bff32c844e052eb7dfc1d465c93c849
windRiderLee <kbntt73@gmail.com>
2024-04-25 11:51:22 +0900

    이응규 기업 퍼포먼스 작업중

--- src/main/java/exdev/com/util/DateUtil.java
+++ src/main/java/exdev/com/util/DateUtil.java
@@ -37,4 +37,42 @@ public class DateUtil {
         return dtFormat.format(cal.getTime());
     }
 
+    /** 
+     * 내용        : 개월수 차이
+     * @생 성 자   : 이응규
+     * @생 성 일자 : 2024. 04. 23 : 최초 생성
+     * @수 정 자   : 
+     * @수 정 일자 :
+     * @수 정 자   :
+     * int result = DateUtil.minusMonth("2024-01-01","2025-01-01");
+     */  
+
+    public static int minusMonth(String startDate, String endDate) throws Exception {
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        
+        // 시작일
+        Date strStartDate = dtFormat.parse(startDate);
+        Calendar calStrDt = Calendar.getInstance();
+        calStrDt.setTime(strStartDate);
+        int startYear  = calStrDt.get(Calendar.YEAR); 
+        int startMonth = calStrDt.get(Calendar.MONTH) +1;
+        int startDay   = calStrDt.get(Calendar.DAY_OF_MONTH);
+            
+        // 종료일
+        Date strEndDate   = dtFormat.parse(endDate);
+        Calendar calEndDt = Calendar.getInstance();
+        calEndDt.setTime(strEndDate);
+        int endYear  = calEndDt.get(Calendar.YEAR); 
+        int endMonth = calEndDt.get(Calendar.MONTH) +1;
+        int endDay   = calEndDt.get(Calendar.DAY_OF_MONTH);
+        
+        // 차이 구하기
+        int diffMonths = endMonth - startMonth;
+        int diffYears = endYear - startYear;
+        int reDate = (diffYears * 12 + diffMonths);
+        
+        return reDate;
+    }
+
 }
