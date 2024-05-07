ce39048e31687d3cd02f5432b45c0e34656c367d
jonsaram <jonsaram74@naver.com>
2024-05-07 18:05:35 +0900

    .

--- src/main/resources/static/ui/js/schedule.js
+++ src/main/resources/static/ui/js/schedule.js
@@ -1,5 +1,10 @@
 var C_CALENDAR = {
-	getScheduleList : function(parm) {
-
+	getScheduleList : function(parm, callback) {
+		
+		dalert(parm);
+		
+		let resultList = [];
+		
+		if(typeof callback == "function") callback(resultList);
 	}
 }
