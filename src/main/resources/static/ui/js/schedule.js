abe84625e68e6528f283a8826df8a00061a54b49
windRiderLee <kbntt73@gmail.com>
2024-06-11 09:58:13 +0900

    이응규 일정관리 조회속도 개선 schedule.js 외부화면에서 일정조회

--- src/main/resources/static/ui/js/schedule.js
+++ src/main/resources/static/ui/js/schedule.js
@@ -1,6 +1,12 @@
 var C_CALENDAR = {
-	getScheduleList : function(parm, callback) {
-		
+     isDayRepeat   : false
+    ,isNotRepeat   : false
+    ,isWeekRepeat  : false
+    ,isMonthRepeat : false
+    ,isYearRepeat  : false
+    ,resultList    : []
+    ,getScheduleList : function(parm, callback) {
+        
         var startDate = parm["startDate"];
         var endDate   = parm["endDate"];
         
@@ -14,24 +20,42 @@ var C_CALENDAR = {
         requestParm["periodYn"]    = "Y";
         requestParm["searchData"]  = parm["searchData"];
         
+        C_CALENDAR.serviceCall(callback, requestParm, "ScheduleService.getTeamDayRepeatSchedule");
+        C_CALENDAR.serviceCall(callback, requestParm, "ScheduleService.getTeamNotRepeatSchedule");
+        C_CALENDAR.serviceCall(callback, requestParm, "ScheduleService.getTeamWeekRepeatSchedule");
+        C_CALENDAR.serviceCall(callback, requestParm, "ScheduleService.getTeamMonthRepeatSchedule");
+        C_CALENDAR.serviceCall(callback, requestParm, "ScheduleService.getTeamYearRepeatSchedule");
         
-        var serviceId = "ScheduleService.getTeamSchedule";
+    }
+    ,serviceCall : function( callback, requestParm, serviceId ) {
+      
         var parm = {
              serviceId              : serviceId
             ,requestParm            : requestParm
         }
-
-        let resultList = [];
-
-        C_COM.requestService(parm, function(resultData) {
-            
-			$.each(resultData.data.list, function() {
-				let item = this;
-				item.START_DATETIME = `${item.SCHEDULE_START_DATE} ${item.START_TIME_MINUTE}:${item.START_TIME_MINUTE}`;
-				resultList.push(item);
-			});
-            if(typeof callback == "function") callback(resultList);
-        });
         
-	}
+        C_COM.requestService(parm, function(resultData) {
+            $.each(resultData.data.list, function() {
+                let item = this;
+                item.START_DATETIME = `${item.SCHEDULE_START_DATE} ${item.START_TIME_MINUTE}:${item.START_TIME_MINUTE}`;
+                C_CALENDAR.resultList.push(item);
+            });
+                      
+            if( serviceId == "ScheduleService.getTeamDayRepeatSchedule"){   C_CALENDAR.isDayRepeat   = true; }
+            if( serviceId == "ScheduleService.getTeamNotRepeatSchedule"){   C_CALENDAR.isNotRepeat   = true; }
+            if( serviceId == "ScheduleService.getTeamWeekRepeatSchedule"){  C_CALENDAR.isWeekRepeat  = true; }
+            if( serviceId == "ScheduleService.getTeamMonthRepeatSchedule"){ C_CALENDAR.isMonthRepeat = true; }
+            if( serviceId == "ScheduleService.getTeamYearRepeatSchedule"){  C_CALENDAR.isYearRepeat  = true; }
+            
+            if( C_CALENDAR.isDayRepeat && C_CALENDAR.isNotRepeat && C_CALENDAR.isWeekRepeat && C_CALENDAR.isMonthRepeat && C_CALENDAR.isYearRepeat ){
+                if(typeof callback == "function") callback(C_CALENDAR.resultList);
+                C_CALENDAR.resultList.length = 0;
+                C_CALENDAR.isDayRepeat   = false;
+                C_CALENDAR.isNotRepeat   = false;
+                C_CALENDAR.isWeekRepeat  = false;
+                C_CALENDAR.isMonthRepeat = false;
+                C_CALENDAR.isYearRepeat  = false;
+            }
+        });
+    }
 }
