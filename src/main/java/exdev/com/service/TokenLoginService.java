12093127acf13ae4f400226bfc275033282325aa
triggerKr <trigger7@naver.com>
2025-07-24 21:52:06 +0900

    토큰 로그인

--- src/main/java/exdev/com/service/TokenLoginService.java
+++ src/main/java/exdev/com/service/TokenLoginService.java
@@ -12,35 +12,47 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service("TokenLoginService")
 public class TokenLoginService {
 	
+    @Autowired
+    private Environment env;
+    
+	public Object autoLogin( Map<String, Object> map) throws Exception{
+		
+		String jwt = getJWT(map);
+        String autoLoginAddress = env.getProperty("spring.autoLogin.address");
+        String loginUrl	= autoLoginAddress+"?token="+jwt; 
+		
+        System.err.println(loginUrl);
+		map.put("loginUrl", loginUrl);
+		return map;
+	}
 	
-	public Object getJWT(Map<String, Object> map) throws Exception {
+	public String getJWT(Map<String, Object> map) throws Exception {
 		
         String secret = "EXmysecretkeymysecretkeymysecretkeyPLAN"; // 32byte 이상
         Key secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
         Instant now = Instant.now();
 
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setSubject("explan")
                 .setIssuedAt(Date.from(now))                  		// 발급 시간 (iat)
                 .setExpiration(Date.from(now.plusSeconds(30)))		// 만료 시간 30초 (exp)
                 .setId(UUID.randomUUID().toString())				// 고유 토큰 ID (jti)
                 .setIssuer("EXPLAN")
                 .claim("id", map.get("id"))
-                .claim("menu", map.get("menu"))
+                .claim("pageId", map.get("pageId"))
                 .claim("param", map.get("jsonParam"))
                 .claim("loginType", map.get("loginType"))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
 
-        System.out.println(jwt);
-		map.put("jwt", jwt);
-		return map;
     }
 	
     // 간단한 클라이언트 IP 추출 함수 (필요에 따라 수정)
